1a841d3cf2a6a562f1bdebad57ee2816bfba337b
elshahawyz <ziad.a.elshahawy@gmail.com>
2023-12-29 16:57:16 -0600

    decreased scrolling time effect

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $('a[href*="#"]').on('click', function (e) {
         {
             scrollTop: $($(this).attr('href')).offset().top,
         },
-        500,
+        300,
         'linear'
     )
 })
